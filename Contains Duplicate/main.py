f6715f740fa52f40a9ff1b2cd5f32173bea0f54c
Rob Dawson <robdawson123.rd@gmail.com>
2025-01-22 15:10:50 +0000

    Fixed Error

--- Contains Duplicate/main.py
+++ Contains Duplicate/main.py
@@ -10,7 +10,7 @@ Brute Force
 def contains_duplicate_brute_force():
     """Loops through nums and returns true if a duplicate and false if there are no duplicates"""
     for i, num1 in enumerate(nums):
-        for _, num2 in enumerate(nums[i + 1], start=i + 1):
+        for _, num2 in enumerate(nums[i + 1 :], start=i + 1):
             if num1 == num2:
                 return True
     return False
