cf9ddfb081b8a72463116195b722edf463efcb35
Rob Dawson <robdawson123.rd@gmail.com>
2025-01-23 10:15:01 +0000

    Passed all test cases Uses one loop

--- Plus One/main.py
+++ Plus One/main.py
@@ -1,4 +1,4 @@
-digits = [1, 2, 3]
+digits = [9, 9, 9]
 
 
 def plus_one():
@@ -18,5 +18,19 @@ def plus_one():
             return
 
 
-plus_one()
+def plus_one_loop():
+    for i in range(len(digits) - 1, -1, -1):
+        if digits[i] == 9:
+            digits[i] = 0
+        else:
+            digits[i] += 1
+            return digits
+    digits.insert(0, 1)
+    return digits
+
+
+# plus_one()
+# print(digits)
+
+plus_one_loop()
 print(digits)
