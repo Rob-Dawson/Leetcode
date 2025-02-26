57413e015fed76f2efa4610c1dd83073f8014ab0
Rob-Dawson <robdawson123.rd@gmail.com>
2025-02-26 00:16:45 +0000

    Solved: Loop through dictionary to find highest occurence. Improvement: Use Max Further Improvement: Use Boyer–Moore voting algorithm

--- Majority Element/main.py
+++ Majority Element/main.py
@@ -1,6 +1,8 @@
 def majorityElement(nums):
     numbers = {}
-    
+    maxNum = 0
+    maxOccurrence = 0
+
     for num in nums:
         if num in numbers:
             numbers[num] +=1
@@ -8,5 +10,10 @@ def majorityElement(nums):
             numbers[num] = 1
     print(numbers)
 
-numbers=[1,2,3,4,5]
-majorityElement(numbers)
+    for num, occurrence in numbers.items():
+        if occurrence > maxOccurrence:
+            maxOccurrence = occurrence
+            maxNum = num
+    return maxNum
+numbers=[1,2,3,3,3,3,4,5]
+print(majorityElement(numbers))
