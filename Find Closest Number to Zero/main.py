4ab273bcb093eebcd6f3783df40502ddffa9b113
Rob-Dawson <robdawson123.rd@gmail.com>
2025-05-12 14:20:29 +0100

    Improved Solution removed the sort

--- Find Closest Number to Zero/main.py
+++ Find Closest Number to Zero/main.py
@@ -8,18 +8,15 @@ Check if the condition exists using absolute
 if exists, take the original number and check if it is greater than closest
 """
 
-nums = [-10, 10, 10, 10]
-# nums = [-4,-2,1,4,8]
+#nums = [-10, 10, -10, 10]
+nums = [-4,-2,1,4,8]
 # nums = [-1,1]
 # nums = [2,1,1,-1,100000]
 closest = nums[0]
-distance = abs(nums[0])
-nums.sort()
-for i, j in enumerate(nums):
-    if abs(j) < abs(closest):
-        closest = j
-        distance = abs(j)
-    if abs(j) == abs(closest):
-        if j > closest:
-            closest = j
+
+for num in nums:
+    if abs(num) < abs(closest):
+        closest = num
+    elif abs(num) == abs(closest) and num > closest:
+        closest = num
 print(closest)
