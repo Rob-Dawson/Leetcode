ef9a2584fb1d09f1c7f26a72d3517f9a46d5e7ff
Rob-Dawson <robdawson123.rd@gmail.com>
2025-05-28 13:33:41 +0100

    Fixed Bug

--- Is Subsequence/main.py
+++ Is Subsequence/main.py
@@ -2,6 +2,10 @@ s = "acb"
 t = "ahbgdc"
 print(len(s))
 def isSub(s,t):
+    if len(s) <= 0:
+        return True
+    if len(t) <= 0:
+        return False
     j = 0
     for i in range(len(t)):
         if t[i] == s[j]:
