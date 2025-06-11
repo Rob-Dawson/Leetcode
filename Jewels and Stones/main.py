ef04050a7490e56ad3450246ed8d3ce7d75b2693
Rob-Dawson <robdawson123.rd@gmail.com>
2025-06-11 09:46:57 +0100

    Solved

--- Jewels and Stones/main.py
+++ Jewels and Stones/main.py
@@ -1,12 +1,12 @@
-jewels = "Z"
+jewels = "zZz"
 stones = "Z"
-jewels_set = set()
 
-for i in jewels:
-    jewels_set.add(i)
+def numJewelsInStones(jewels, stones):
+    jewels_set = set(jewels)
 
-jewel_total = 0
-for i in stones:
-    if i in jewels_set:
-        jewel_total += 1
-print(jewel_total)
+    jewel_total = 0
+    for i in stones:
+        if i in jewels_set:
+            jewel_total += 1
+    return jewel_total
+print(numJewelsInStones(jewels, stones))
